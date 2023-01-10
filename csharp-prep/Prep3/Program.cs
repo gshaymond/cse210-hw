6cb7320b305d1c454af7f37b3efae9dbeab6eeec
gshaymond <gshaymond@byui.edu>
2023-01-10 14:57:27 -0700

    add prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int chosen = randomGenerator.Next(1, 2000000);
+        Console.WriteLine("A number has been chosen.");
+            
+        // do
+        // {
+        //     Console.Write("Make your guess from 1 to 2,000,000. ");
+        //     guess 
+
+        // } while (guess != chosen);
+        
+
     }
 }
