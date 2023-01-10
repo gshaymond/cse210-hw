28d8347dd0ab44d0fcd28e178091f826e94a6ff5
gshaymond <gshaymond@byui.edu>
2023-01-10 15:54:32 -0700

    add program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,19 +2,34 @@ using System;
 
 class Program
 {
+    private static int guess;
+    
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
         int chosen = randomGenerator.Next(1, 2000000);
         Console.WriteLine("A number has been chosen.");
-            
-        // do
-        // {
-        //     Console.Write("Make your guess from 1 to 2,000,000. ");
-        //     guess 
+        int guess = 0;
 
-        // } while (guess != chosen);
-        
+
+        while (guess != chosen)
+        {
+            Console.Write("Make your guess from 1 to 2,000,000. ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            if (guess > chosen)
+            {
+                Console.WriteLine("Guess Lower.");
+            }
+            else if (guess < chosen)
+            {
+                Console.WriteLine("Guess Higher.");
+            }
+            else
+            {
+                Console.WriteLine($"Correct, the number is {chosen}");
+            }
+        }
 
     }
 }
