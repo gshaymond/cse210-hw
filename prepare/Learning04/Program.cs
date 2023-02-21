9917ec8c5c361656e48555d3a0c885c42c4241b8
gshaymond <gshaymond@byui.edu>
2023-02-20 18:52:58 -0700

    update program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samual Bennet", "Multiplication");
+        Console.WriteLine();
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+        
+        Math a2 = new Math("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine();
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        Writing a3 = new Writing("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine();
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInfo());
+        Console.WriteLine();
     }
 }
---
+++ prepare/Learning04/Writing.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Writing : Assignment
+{
+    private string _title = "";
+
+    public Writing(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInfo()
+    {
+        string studentName = GetStudentName();
+        return ($"{_title} by {studentName}");
+    }
+}
