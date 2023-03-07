5bf7587b8a66d883439d9d135061986de80a1296
gshaymond <gshaymond@byui.edu>
2023-03-06 18:58:43 -0700

    update program

---
+++ Follow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test1
+{
+    class BreathingCommand : Command
+    {
+        public void Execute()
+        {
+            throw new Exception("");
+        }
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue", 7);
+        // Console.WriteLine(shape1.GetColor());
+        // Console.WriteLine(shape1.GetArea());
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Green", 4, 6);
+        // Console.WriteLine(shape2.GetColor());
+        // Console.WriteLine(shape2.GetArea());
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Purple", 9);
+        // Console.WriteLine(shape3.GetColor());
+        // Console.WriteLine(shape3.GetArea());
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
